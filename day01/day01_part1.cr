8caf5b5bab88a89ba8458e849d2c0df48b9fb687
cartermp <pcarter@fastmail.com>
2021-12-01 10:23:29 -0800

    better code

--- day01/day01_part1.cr
+++ day01/day01_part1.cr
@@ -1,16 +1,11 @@
-def lines
-    File.open("data/day01/data.txt").each_line.map(&.to_i).to_a
-end
+lines = File.open("data/day01/data.txt").each_line.map(&.to_i)
 
-prev = lines[0]
 count = 0
 
-lines.each do |line|
-  if line > prev
+lines.each_cons_pair do |a, b|
+  if a < b
     count += 1
   end
-
-  prev = line
 end
 
 puts count
