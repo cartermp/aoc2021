e2634376a7e5cf1f96790ec1986f45261204afdd
cartermp <pcarter@fastmail.com>
2021-12-07 21:06:51 -0800

    nicer

--- day07/day07_part1.cr
+++ day07/day07_part1.cr
@@ -1,16 +1,11 @@
 crab_positions =
     File.read("data/day07/data.txt").split(',')
         .map {|num| num.to_i }
-        .sort
 
 total_fuel = 2147483647
 
-(crab_positions[0]..crab_positions[crab_positions.size-1]).each do |pos|
-    fuel = 0
-    crab_positions.each do |crab_position|
-        fuel += (crab_position - pos).abs
-    end
-    
+(crab_positions.min..crab_positions.max).each do |pos|
+    fuel = crab_positions.reduce(0) {|sum, c_pos| sum + (c_pos - pos).abs }
     total_fuel = Math.min(total_fuel, fuel)
 end
 
--- day07/day07_part2.cr
+++ day07/day07_part2.cr
@@ -1,17 +1,15 @@
 crab_positions =
-    File.read("data/day07/data.txt").split(',')
+    File.read("data/day07/test.txt").split(',')
         .map {|num| num.to_i }
         .sort
 
 total_fuel = 2147483647
 
 (crab_positions[0]..crab_positions[crab_positions.size-1]).each do |pos|
-    fuel = 0
-    crab_positions.each do |crab_position|
-        dist = (pos - crab_position).abs
-        fuel += (dist * (dist + 1)) / 2
-    end
-    
+    fuel = crab_positions.reduce(0) { |sum, c_pos|
+        dist = (pos - c_pos).abs
+        sum + (dist * (dist + 1)) / 2
+    }
     total_fuel = Math.min(total_fuel, fuel)
 end
 
